653b3a143b80ebf840857bdbd52df5bd29302d33
alois <alois.ceola@hotmail.fr>
2022-12-22 16:18:57 +0100

    feat: lib now support monorepo structure

--- src/fixtures.module.ts
+++ src/fixtures.module.ts
@@ -47,4 +47,49 @@ export class FixturesModule {
             exports: ['FIXTURES'],
         }
     }
+
+    static async forRootAsyncMonorepo(fixturesPathPattern: string, entitiesPathPattern: string, buildDir: string = 'dist'): Promise<DynamicModule> {
+        // import fixtures
+        const fixturesPath = glob.sync(fixturesPathPattern)
+        const fixturesRelativePath = fixturesPath.map((path) => `${process.cwd()}/${buildDir}/${path}`).map((path) => path.replace('.ts', ''))
+        const fixturesProviders: any[] = []
+        const importedFixtures = await Promise.all(fixturesRelativePath.map((path) => import(path)))
+
+        importedFixtures.forEach((fixture) => {
+            fixturesProviders.push(fixture[Object.keys(fixture)[0]])
+        })
+
+
+        // import entities
+        const entitiesPath = glob.sync(entitiesPathPattern)
+        const entitiesRelativePath = entitiesPath.map((path) => `${process.cwd()}/${buildDir}/${path}`).map((path) => path.replace('.ts', ''))
+        const entitiesProviders: any[] = []
+        const importedEntities = await Promise.all(entitiesRelativePath.map((path) => import(path)))
+
+        importedEntities.forEach((entity) => {
+            entitiesProviders.push({
+                name: entity[Object.keys(entity)[0]].name,
+                schema: entity[Object.keys(entity)[1]],
+            })
+        })
+
+        return {
+            module: FixturesModule,
+            imports: [MongooseModule.forFeature(entitiesProviders)],
+            providers: [
+                ReferenceRepository,
+                ...fixturesProviders,
+                {
+                    provide: 'FIXTURES',
+                    useFactory(...args) {
+                        const providersObject = {}
+                        args.forEach((arg) => (providersObject[arg.constructor.name] = arg))
+                        return providersObject
+                    },
+                    inject: fixturesProviders,
+                },
+            ],
+            exports: ['FIXTURES'],
+        }
+    }
 }
